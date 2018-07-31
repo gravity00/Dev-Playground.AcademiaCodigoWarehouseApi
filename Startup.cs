666a775ae600ec70a18176cf01dcadfeb78054cc
João Simões <nogravity00@outlook.com>
2018-07-31 18:51:22 +0100

    Seed na DB in memory

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,57 @@ namespace AcademiaCodigoWarehouseApi {
             }
 
             app.UseMvcWithDefaultRoute ();
+
+            using(var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope()){
+                SeedDummyData(scope.ServiceProvider.GetRequiredService<WarehouseContext>());
+            }
+        }
+
+        private void SeedDummyData(WarehouseContext ctx){
+            var dummyData = new []{
+                new ProductEntity {
+                    Id = 1,
+                    Code = "11111",
+                    Name = "Bola de Praia",
+                    Description = "Bola de praia maravilhosa",
+                    Price = 10.5m,
+                    StockMovements ={
+                        new StockMovementEntity{
+                            Price = 10.5m,
+                            Quantity = -2,
+                            CreatedOn = DateTimeOffset.Now.AddDays(-2),
+                            CreatedBy = "joao.simoes"
+                        },
+                        new StockMovementEntity{
+                            Price = 9m,
+                            Quantity = 10,
+                            CreatedOn = DateTimeOffset.Now.AddDays(-3),
+                            CreatedBy = "joao.simoes"
+                        }
+                    },
+                    CreatedOn = DateTimeOffset.Now.AddDays(-3),
+                    CreatedBy = "joao.simoes",
+                    UpdatedOn = DateTimeOffset.Now.AddMinutes (-32),
+                    UpdatedBy = "joao.simoes"
+                },
+                new ProductEntity {
+                    Id = 2,
+                    Code = "22222",
+                    Name = "Bola de Futebol",
+                    Description = "Esta bola nem é assim tão boa",
+                    Price = 50m,
+                    CreatedOn = DateTimeOffset.Now.AddDays(-3),
+                    CreatedBy = "joao.simoes",
+                    UpdatedOn = DateTimeOffset.Now.AddHours (-5),
+                    UpdatedBy = "renato.verissimo"
+                }
+            };
+
+            foreach(var p in dummyData){
+                ctx.Add(p);
+            }
+
+            ctx.SaveChanges();
         }
     }
 }
