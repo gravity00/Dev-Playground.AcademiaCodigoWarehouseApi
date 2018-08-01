9548a1a26c021679573d6a199613ff3c75e2e416
João Simões <nogravity00@outlook.com>
2018-08-01 16:38:11 +0100

    Alinhar precisão do decimal

--- Database/WarehouseContext.cs
+++ Database/WarehouseContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 namespace AcademiaCodigoWarehouseApi.Database {
     public class WarehouseContext : DbContext {
         public WarehouseContext (DbContextOptions<WarehouseContext> options) : base (options) {
-            
+
         }
 
         protected override void OnModelCreating (ModelBuilder builder) {
@@ -18,7 +18,7 @@ namespace AcademiaCodigoWarehouseApi.Database {
                 cfg.Property (e => e.Code).IsRequired ().HasMaxLength (5);
                 cfg.Property (e => e.Name).IsRequired ().HasMaxLength (128);
                 cfg.Property (e => e.Description).HasMaxLength (1024);
-                cfg.Property (e => e.Price).IsRequired ();
+                cfg.Property (e => e.Price).IsRequired ().HasColumnType ("decimal(15,3)");
 
                 cfg.Property (e => e.CreatedOn).IsRequired ();
                 cfg.Property (e => e.CreatedBy).IsRequired ().HasMaxLength (128);
@@ -39,7 +39,7 @@ namespace AcademiaCodigoWarehouseApi.Database {
                 cfg.HasKey (e => e.Id);
 
                 cfg.Property (e => e.Id).IsRequired ().ValueGeneratedOnAdd ();
-                cfg.Property (e => e.Price).IsRequired ();
+                cfg.Property (e => e.Price).IsRequired ().HasColumnType ("decimal(15,3)");
                 cfg.Property (e => e.Quantity).IsRequired ();
 
                 cfg.Property (e => e.CreatedOn).IsRequired ();
