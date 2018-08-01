4cf66028c117540fac5185066ac37afcd57540ce
João Simões <nogravity00@outlook.com>
2018-08-01 11:35:58 +0100

    Configuração de MySQL

--- Startup.cs
+++ Startup.cs
@@ -8,12 +8,13 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using MySql.Data.MySqlClient;
 
 namespace AcademiaCodigoWarehouseApi {
     public class Startup {
         public void ConfigureServices (IServiceCollection services) {
             services.AddDbContext<WarehouseContext> (builder => {
-                builder.UseInMemoryDatabase("Warehouse");
+                builder.UseMySql("server=localhost;uid=warehouse-api;pwd=123456;database=Warehouse");
             });
 
             services.AddMvc ();
@@ -27,53 +28,8 @@ namespace AcademiaCodigoWarehouseApi {
             app.UseMvcWithDefaultRoute ();
 
             using(var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope()){
-                SeedDummyData(scope.ServiceProvider.GetRequiredService<WarehouseContext>());
+                scope.ServiceProvider.GetRequiredService<WarehouseContext>().Database.EnsureCreated();
             }
-        }
-
-        private void SeedDummyData(WarehouseContext ctx){
-            var dummyData = new []{
-                new ProductEntity {
-                    Code = "11111",
-                    Name = "Bola de Praia",
-                    Description = "Bola de praia maravilhosa",
-                    Price = 10.5m,
-                    StockMovements ={
-                        new StockMovementEntity{
-                            Price = 10.5m,
-                            Quantity = -2,
-                            CreatedOn = DateTimeOffset.Now.AddDays(-2),
-                            CreatedBy = "joao.simoes"
-                        },
-                        new StockMovementEntity{
-                            Price = 9m,
-                            Quantity = 10,
-                            CreatedOn = DateTimeOffset.Now.AddDays(-3),
-                            CreatedBy = "joao.simoes"
-                        }
-                    },
-                    CreatedOn = DateTimeOffset.Now.AddDays(-3),
-                    CreatedBy = "joao.simoes",
-                    UpdatedOn = DateTimeOffset.Now.AddMinutes (-32),
-                    UpdatedBy = "joao.simoes"
-                },
-                new ProductEntity {
-                    Code = "22222",
-                    Name = "Bola de Futebol",
-                    Description = "Esta bola nem é assim tão boa",
-                    Price = 50m,
-                    CreatedOn = DateTimeOffset.Now.AddDays(-3),
-                    CreatedBy = "joao.simoes",
-                    UpdatedOn = DateTimeOffset.Now.AddHours (-5),
-                    UpdatedBy = "renato.verissimo"
-                }
-            };
-
-            foreach(var p in dummyData){
-                ctx.Add(p);
-            }
-
-            ctx.SaveChanges();
         }
     }
 }
